454f8ebbd8551fe61da75fdec57d80092cd98226
Marc Zwiers <marczwiers@pm.me>
2026-03-06 12:40:54 +0100

    initial commin

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <h1>Hello Vue 🚀</h1>
+  </main>
+</template>
+
+<style scoped>
+main {
+  padding: 2rem;
+  font-family: system-ui;
+}
+</style>
