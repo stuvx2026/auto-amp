b5a012c62f30d75126405fe9c69a6cce046d57ea
Marc Zwiers <marczwiers@pm.me>
2026-03-06 13:45:19 +0100

    yodated vite.config

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
-<script setup>
-
-</script>
-
+<!-- src/App.vue -->
 <template>
   <main>
     <h1>Hello Vue TEST 🚀</h1>
   </main>
 </template>
+
+<script setup>
+</script>
 
 <style scoped>
 main {
