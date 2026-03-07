c010cfbf6c2a785ffabe7ad16bf38df7b96e866e
Marc Zwiers <marczwiers@pm.me>
2026-03-07 11:42:06 +0100

    fixed config

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,19 @@
-np
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/Auto-AMP/', // <-- add this line! Replace 'auto-amp' with your repo name
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+})
